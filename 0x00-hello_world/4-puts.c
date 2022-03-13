6684e596c7a89f820433952ba6ba01f7b154b73e
Emerie111 <emeriemadu00@gmail.com>
2022-03-13 05:48:47 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
-#1/bin/bash
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
-
- *main -entry point                                                                                                                     
-
- *                                                                                                                                      
-
- *Returns: 0 after printing the function                                                                                                
-
- */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
-{puts("\"Programming is like building a multilingual puzzle");                                                                          
-
-returns (0);                                                                                                                            
-
-}                         
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzlee");
+return (0);
+}
