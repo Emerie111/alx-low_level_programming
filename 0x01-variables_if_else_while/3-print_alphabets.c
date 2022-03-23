a53c0a6a429a24282778db65c6e23a28c059b35b
Emerie111 <emeriemadu00@gmail.com>
2022-03-22 23:29:32 -0700

    all variables task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- *Return: Always 0 (Success/correct)
+ * Return: Always 0 (Sucess/correct)
  */
 int main(void)
 
@@ -10,17 +10,21 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (upper_case = 'A': upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
-
-for (lower_case = 'a' lower_case <= 'z': lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
 
-putchar('\n');
+
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+
+
+
+
+putchar ('\n');
 
 return (0);
 
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
+
 int main(void)
 
 {
 char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
 {
 putchar(alphabet);
 }
@@ -17,5 +18,6 @@ putchar(alphabet);
 putchar('\n');
 
 return (0);
+
 
 }
